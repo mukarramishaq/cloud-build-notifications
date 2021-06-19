5f879f73a4ec86935dd8c166e1d3907b5441bc9f
Mukarram Ishaq <mukarramishaq189@gmail.com>
2021-06-20 02:31:52 +0500

    enriched the chat notification content and minor tweaks

--- src/clients/GoogleChatRoom/GoogleChatRoomClient.ts
+++ src/clients/GoogleChatRoom/GoogleChatRoomClient.ts
@@ -6,14 +6,22 @@ import {
     GOOGLE_CHAT_ROOM_WEBHOOK,
     PROJECT_LOGO_URL,
     PROJECT_NAME,
-    INSTANCE_NAME
+    MESSAGE_SUBTITLE
 } from "../../config/env";
+import formatDistance from "date-fns/formatDistance";
 class GoogleChatRoomClient extends Client<MessageType> {
 
     protected repoName: string;
     protected branchName: string;
+    protected shortSHA: string;
+    protected commitSHA: string;
+    protected revision: string;
+    protected trigger: string;
     protected logUrl: string;
     protected status: Status;
+    protected startAt: Date;
+    protected endAt: Date;
+    protected timeSpan: string;
     protected buildResource: BuildResourceType
 
 
@@ -43,16 +51,23 @@ class GoogleChatRoomClient extends Client<MessageType> {
         this.buildResource = JSON.parse(Buffer.from(this.message.message.data, 'base64').toString());
         this.repoName = this.buildResource.substitutions.REPO_NAME;
         this.branchName = this.buildResource.substitutions.BRANCH_NAME;
+        this.shortSHA = this.buildResource.substitutions.SHORT_SHA;
+        this.commitSHA = this.buildResource.substitutions.COMMIT_SHA;
+        this.revision = this.buildResource.substitutions.REVISION_ID;
+        this.trigger = this.buildResource.substitutions.TRIGGER_NAME;
+        this.startAt = new Date(this.buildResource.startTime);
+        this.endAt = this.buildResource.finishTime && new Date(this.buildResource.finishTime);
+        this.timeSpan = formatDistance(this.endAt || new Date(), this.startAt, { includeSeconds: true });
         this.status = this.buildResource.status;
         this.logUrl = this.buildResource.logUrl;
     }
 
-    protected getProjectName() {
-        return PROJECT_NAME || this.repoName;
+    protected getTitle() {
+        return `${this.repoName}  >>  ${this.branchName}`;
     }
 
-    protected getInstanceName() {
-        return INSTANCE_NAME || this.branchName;
+    protected getSubtitle() {
+        return MESSAGE_SUBTITLE || PROJECT_NAME;
     }
 
     protected getIconUrl() {
@@ -76,8 +91,8 @@ class GoogleChatRoomClient extends Client<MessageType> {
             cards: [
                 {
                     header: {
-                        title: this.getProjectName(),
-                        subtitle: this.getInstanceName(),
+                        title: this.getTitle(),
+                        subtitle: this.getSubtitle(),
                         imageUrl: this.getProjectLogUrl()
                     },
                     sections: [
@@ -105,11 +120,60 @@ class GoogleChatRoomClient extends Client<MessageType> {
                         {
                             widgets: [
                                 {
+                                    keyValue: {
+                                        topLabel: "Repository",
+                                        content: this.repoName,
+                                    }
+                                },
+                                {
+                                    keyValue: {
+                                        topLabel: "Branch",
+                                        content: this.branchName,
+                                    }
+                                },
+                                {
+                                    keyValue: {
+                                        topLabel: "Commit SHA",
+                                        content: this.shortSHA
+                                    }
+                                },
+                                {
+                                    keyValue: {
+                                        topLabel: "Revision ID",
+                                        content: this.revision
+                                    }
+                                },
+                                {
+                                    keyValue: {
+                                        topLabel: "Trigger Name",
+                                        content: this.trigger
+                                    }
+                                },
+                                {
+                                    keyValue: {
+                                        topLabel: "Start Time",
+                                        content: this.startAt.toUTCString()
+                                    }
+                                },
+                                {
+                                    keyValue: {
+                                        topLabel: "Finish Time",
+                                        content: (this.endAt && this.endAt.toUTCString()) || "Unknown"
+                                    }
+                                },
+                                {
+                                    keyValue: {
+                                        topLabel: "Elapsed Time",
+                                        content: this.timeSpan
+                                    }
+                                },
+                                {
                                     textParagraph: {
                                         text: "<font color='#B3B3B3'>made with sheer <font color=\"#ff0000\"><b>&hearts;<b></font> <font color='#B3B3B3'>by <a href='https://github.com/mukarramishaq'>MKDEBUG</a></font>"
                                     }
                                 }
-                            ]
+                            ],
+                            collapsable: true
                         }
                     ]
                 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
 config();
 
 export const GOOGLE_CHAT_ROOM_WEBHOOK = process.env.GOOGLE_CHAT_ROOM_WEBHOOK;
 export const PROJECT_NAME = process.env.PROJECT_NAME;
-export const INSTANCE_NAME = process.env.INSTANCE_NAME;
 export const PROJECT_LOGO_URL = process.env.PROJECT_LOGO_URL;
+export const MESSAGE_SUBTITLE = process.env.MESSAGE_SUBTITLE;
