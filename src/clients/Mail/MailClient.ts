a8956d2cd67843e3b62d799868d4e0c6e3e36964
Mukarram Ishaq <mukarramishaq189@gmail.com>
2021-08-07 14:13:46 +0500

    fix: change structure to smtp credentials to support both login and oauth2 methods

--- src/clients/Mail/MailClient.ts
+++ src/clients/Mail/MailClient.ts
@@ -3,21 +3,20 @@ import { MessageType } from "../types";
 import {
     PROJECT_NAME,
     MESSAGE_SUBTITLE,
-    SMTP_CONNECTION_URI,
+    SMTP_CREDENTIALS,
 } from "../../config/env";
 import nodemailer, { SentMessageInfo } from "nodemailer";
 
 export class MailClient extends Client {
     protected transporter: nodemailer.Transporter<SentMessageInfo>;
-
     constructor() {
         super();
-        this.transporter = nodemailer.createTransport(SMTP_CONNECTION_URI);
+        this.transporter = nodemailer.createTransport(SMTP_CREDENTIALS as any);
     }
 
     async send() {
         return await this.transporter.sendMail({
-            from: "Mukarram Ishaq <mukarram.ishaq@rolustech.net>",
+            from: "Mukarram Ishaq Jutt <mukarramishaq190@gmail.com>",
             to: "MKDEBUG <mukarramishaq189@gmail.com>",
             subject: `CICD | ${this.repoName} | ${this.branchName} | ${this.status}`,
             text: `Hello there`,
@@ -25,6 +24,10 @@ export class MailClient extends Client {
     }
 
     public isActive() {
-        return !!SMTP_CONNECTION_URI;
+        return (
+            !!SMTP_CREDENTIALS &&
+            !!SMTP_CREDENTIALS.host &&
+            !!SMTP_CREDENTIALS.port
+        );
     }
 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,28 @@
 import { config } from "dotenv";
+import { TransportOptions } from "nodemailer";
 config();
-
-export const GOOGLE_CHAT_ROOM_WEBHOOK = process.env.GOOGLE_CHAT_ROOM_WEBHOOK;
-export const SMTP_CONNECTION_URI = process.env.SMTP_CONNECTION_URI;
 
 export const PROJECT_NAME = process.env.PROJECT_NAME;
 export const MESSAGE_SUBTITLE = process.env.MESSAGE_SUBTITLE;
+
+/**
+ * Google Chat Room Credetials
+ */
+export const GOOGLE_CHAT_ROOM_WEBHOOK = process.env.GOOGLE_CHAT_ROOM_WEBHOOK;
+
+/**
+ * SMTP Credentials
+ */
+export const SMTP_CREDENTIALS = {
+    host: process.env.SMTP_HOST,
+    port: +(process.env.SMTP_PORT),
+    auth: {
+        type: process.env.SMTP_AUTH_TYPE,
+        user: process.env.SMTP_AUTH_USER,
+        pass: process.env.SMTP_AUTH_LOGIN_PASS,
+        method: process.env.SMTP_AUTH_CUSTOM_METHOD,
+        clientId: process.env.SMTP_AUTH_OAUTH2_CLIENT_ID,
+        clientSecret: process.env.SMTP_AUTH_OAUTH2_CLIENT_SECRET,
+        refreshToken: process.env.SMTP_AUTH_OAUTH2_REFRESH_TOKEN,
+    },
+};
